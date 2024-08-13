91ff00d964c7e3c6a680cbc489bd8e64b7d56868
Caio <51968701+caiosrs@users.noreply.github.com>
2024-08-13 16:32:52 -0300

    Delete alocacao_pura directory

--- alocacao_pura/alocacao_pura/urls.py
+++
@@ -1,9 +0,0 @@
-#alocacao_pura/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('calculos.urls')),
-]
